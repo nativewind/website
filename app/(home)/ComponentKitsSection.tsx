8c52ba0bb0139055935ad5feef15104ceab7a086
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-05-30 20:33:20 -0400

    fixes #10, better align section paddings on landing

--- app/(home)/ComponentKitsSection.tsx
+++ app/(home)/ComponentKitsSection.tsx
@@ -8,9 +8,9 @@ export default function ComponentKitsSection() {
 
       <section className="relative flex flex-col w-full max-w-fd-container mx-auto -mt-[1px] border-y border-dashed">    
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-left">
-          <a href="https://nativewindui.com" target="_blank" rel="noopener noreferrer" className="p-10 pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group border-r border-dashed relative">
+          <a href="https://nativewindui.com" target="_blank" rel="noopener noreferrer" className="p-8 lg:p-10 lg:pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group border-r border-dashed relative">
             <div className="absolute inset-0 -z-10 bg-grid-dots-current group-hover:text-cyan-400/40 h-full w-full text-transparent duration-300" />
-            <div className="-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
+            <div className="lg:-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
               <img className="rounded-sm group-hover:rounded-lg duration-600" src="https://nativewindui.com/og.png" />
             </div>
             <h2 className="text-lg font-semibold mt-8 relative flex gap-2">NativewindUI
@@ -18,9 +18,9 @@ export default function ComponentKitsSection() {
             </h2>
             <p className="text-sm opacity-50 mt-4 text-pretty">30+ beautiful components and flows that strive for a native look & feel. The perfect starting point for those who need to ship fast and look good doing it.</p>
           </a>
-          <a href="https://rnr-docs.vercel.app/" target="_blank" rel="noopener noreferrer" className="p-10 pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group border-r border-dashed relative">
+          <a href="https://rnr-docs.vercel.app/" target="_blank" rel="noopener noreferrer" className="p-8 lg:p-10 lg:pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group border-r border-dashed relative">
             <div className="absolute inset-0 -z-10 bg-grid-dots-current group-hover:text-cyan-400/40 h-full w-full text-transparent duration-300" />
-            <div className="-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
+            <div className="lg:-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
               <img className="rounded-sm group-hover:rounded-lg duration-600" src="https://rnr-docs.vercel.app/og.jpeg" />
             </div>
             <h2 className="text-lg font-semibold mt-8 relative flex gap-2">React Native Reusables
@@ -28,9 +28,9 @@ export default function ComponentKitsSection() {
             </h2>
             <p className="text-sm opacity-50 mt-4 text-pretty">Build your own component libraries with react-native-reusables. Copy, paste, and tailor the code to suit your specific requirements.</p>
           </a>
-          <a href="https://gluestack.io/" target="_blank" rel="noopener noreferrer" className="p-10 pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group max-lg:border-r max-lg:border-t border-dashed relative">
+          <a href="https://gluestack.io/" target="_blank" rel="noopener noreferrer" className="p-8 lg:p-10 lg:pb-8 flex-1 hover:bg-black/5 dark:hover:bg-cyan-400/5 duration-300 group max-lg:border-r max-lg:border-t border-dashed relative">
             <div className="absolute inset-0 -z-10 bg-grid-dots-current group-hover:text-cyan-400/40 h-full w-full text-transparent duration-300" />
-            <div className="-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
+            <div className="lg:-m-2 border border-dashed rounded-md backdrop-blur group-hover:rounded-xl duration-600 ease-out p-2 group-hover:border-black dark:group-hover:border-cyan-400">
               <img className="rounded-sm group-hover:rounded-lg duration-600" src="https://gluestack.io/images/og-image.png" />
             </div>
             <h2 className="text-lg font-semibold mt-8 relative flex gap-2">Gluestack
--- app/(home)/SectionLink.tsx
+++ app/(home)/SectionLink.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/cn";
 
 export default function SectionLink({href, name, border, className}: {href: string, name: string, border?: boolean, className?: string}) {
   return (
-    <div className={cn(className, `sticky w-full max-w-fd-container top-14 mx-auto text-fd-muted-foreground/50 text-xs text-left font-mono ${ border && 'pl-1'} max-md:hidden`)}>
+    <div className={cn(className, `sticky w-full max-w-fd-container top-14 mx-auto text-fd-muted-foreground/50 text-xs text-left font-mono ${ border && 'pl-1'}`)}>
       <a href={href} className="group w-fit">
         {/* -rotate-90 block -translate-x-[calc(50%+1rem)] translate-y-[calc(100%+5rem)] */}
         <div className={`opacity-0 group-hover:opacity-100 duration-300 absolute left-0 pr-2 pl-1.5 -translate-x-full ${border && 'border-l border-y -bottom-[1px] border-dashed'}`}>
