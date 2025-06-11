5f1c91e1ce22b4b2448938c7785d8316f1b6efa9
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-06-11 14:55:33 -0400

    tweak the play button animation, adjust decoration on community page, lowercase gluestack

--- app/(home)/ComponentKitsSection.tsx
+++ app/(home)/ComponentKitsSection.tsx
@@ -10,7 +10,7 @@ export default function ComponentKitsSection() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-left">
           <UIKitCard url="https://nativewindui.com" title="NativewindUI" preview="https://nativewindui.com/og.png" description="30+ beautiful components and flows that strive for a native look & feel. The perfect starting point for those who need to ship fast and look good doing it." />
           <UIKitCard url="https://rnr-docs.vercel.app/" title="React Native Reusables" preview="https://rnr-docs.vercel.app/og.jpeg" description="Build your own component libraries with react-native-reusables. Copy, paste, and tailor the code to suit your specific requirements." />
-          <UIKitCard url="https://gluestack.io/" title="Gluestack" preview="https://gluestack.io/images/og-image.png" description="Comprehensive React and React Native component library for building modern, high-performance web and mobile apps. Copy-paste UI components library & patterns crafted with Tailwind CSS (Nativewind)." />
+          <UIKitCard url="https://gluestack.io/" title="gluestack" preview="https://gluestack.io/images/og-image.png" description="Comprehensive React and React Native component library for building modern, high-performance web and mobile apps. Copy-paste UI components library & patterns crafted with Tailwind CSS (Nativewind)." />
         </div>
       </section>
     </>
