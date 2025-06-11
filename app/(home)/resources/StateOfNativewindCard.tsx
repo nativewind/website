3dadaca96e6cd55690d48fa077715196a87c54d1
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-06-11 14:55:33 -0400

    tweak the play button animation, adjust decoration on community page, lowercase gluestack

--- app/(home)/resources/StateOfNativewindCard.tsx
+++ app/(home)/resources/StateOfNativewindCard.tsx
@@ -13,7 +13,7 @@ export default function StateOfNWCard({ title, image, date, time, url }: { title
         <div className="absolute top-1/2 left-1/2 -translate-1/2 rounded-full bg-gradient-to-br from-fd-background to-fd-accent p-2 opacity-0 group-hover:opacity-100 scale-75 group-hover:scale-100 ease-[cubic-bezier(0.175,0.885,0.32,1.275)] duration-300 z-10 overflow-clip">
           {/* shimmer */}
           <div className="w-2 h-full rotate-45 bg-none dark:bg-white/50 absolute -top-7 -left-2 delay-400 group-hover:duration-1000 group-hover:translate-16 blur-sm" />
-          <Play className="fill-current translate-x-0.5 size-10 stroke-none text-fd-primary drop-shadow-2xl drop-shadow-fd-primary scale-75 delay-150 duration-300 group-hover:scale-100 ease-[cubic-bezier(0.175,0.885,0.12,1.575)]" />
+          <Play className="fill-current translate-x-0.5 size-10 stroke-none text-fd-primary drop-shadow-2xl drop-shadow-fd-primary scale-75 delay-100 duration-300 group-hover:scale-100 ease-[cubic-bezier(0.175,0.885,0.33,1.275)]" />
         </div>
         <Image src={image} width={1280} height={720} alt="State of Nativewind Thumbnail" className="aspect-video h-auto w-76 rounded-2xl group-hover:opacity-50 transition-opacity duration-300 object-cover" />
         <div className="absolute bottom-2 right-2 rounded-full bg-fd-muted px-1.5 py-0.5 text-xs">
