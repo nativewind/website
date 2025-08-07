c3a4e2aa892efcbf6ec86b9149728c4c9fb54040
PickleNik <31113245+PickleNik@users.noreply.github.com>
2025-08-07 16:54:13 -0400

    Update state-of-nw.ts

--- app/(home)/resources/state-of-nw.ts
+++ app/(home)/resources/state-of-nw.ts
@@ -18,7 +18,7 @@ export const stateOfNativewind = [
     title: "State of Nativewind",
     image: StateOfNWNewest,
     date: "2025-08-05",
-    time: "UPCOMING",
+    time: "32:59",
     url: "https://www.youtube.com/watch?v=chbcOn801kg",
   },
   {
