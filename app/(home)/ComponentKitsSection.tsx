0f388a146313f2c0697a8ca0d213dfeba00e56a6
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-08-27 17:44:08 -0400

    fix: fix & format new RnR OG source

--- app/(home)/ComponentKitsSection.tsx
+++ app/(home)/ComponentKitsSection.tsx
@@ -6,13 +6,29 @@ export default function ComponentKitsSection() {
     <>
       <SectionTitle id="component-kits" title="UI Component Kits" />
 
-      <section className="relative flex flex-col w-full max-w-fd-container mx-auto -mt-[1px] border-y border-dashed">    
+      <section className="relative flex flex-col w-full max-w-fd-container mx-auto -mt-[1px] border-y border-dashed">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-left">
-          <UIKitCard url="https://nativewindui.com" title="NativewindUI" preview="https://nativewindui.com/og.png" description="30+ beautiful components and flows that strive for a native look & feel. The perfect starting point for those who need to ship fast and look good doing it." />
-          <UIKitCard url="https://rnr-docs.vercel.app/" title="React Native Reusables" preview="https://rnr-docs.vercel.app/og.jpeg" description="Build your own component libraries with react-native-reusables. Copy, paste, and tailor the code to suit your specific requirements." />
-          <UIKitCard url="https://gluestack.io/" title="gluestack" preview="https://gluestack.io/images/og-image.png" description="Comprehensive React and React Native component library for building modern, high-performance web and mobile apps. Copy-paste UI components library & patterns crafted with Tailwind CSS (Nativewind)." />
+          <UIKitCard
+            url="https://nativewindui.com"
+            title="NativewindUI"
+            preview="https://nativewindui.com/og.png"
+            description="30+ beautiful components and flows that strive for a native look & feel. The perfect starting point for those who need to ship fast and look good doing it."
+          />
+          <UIKitCard
+            invertOg
+            url="https://rnr-docs.vercel.app/"
+            title="React Native Reusables"
+            preview="https://rnr-docs.vercel.app/og.png"
+            description="Build your own component libraries with react-native-reusables. Copy, paste, and tailor the code to suit your specific requirements."
+          />
+          <UIKitCard
+            url="https://gluestack.io/"
+            title="gluestack"
+            preview="https://gluestack.io/images/og-image.png"
+            description="Comprehensive React and React Native component library for building modern, high-performance web and mobile apps. Copy-paste UI components library & patterns crafted with Tailwind CSS (Nativewind)."
+          />
         </div>
       </section>
     </>
-  )
+  );
 }
